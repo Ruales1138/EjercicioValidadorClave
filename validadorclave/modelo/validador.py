c0edffc9b33536466cd9be5d0b84cecdbeb454b5
Thelastpepas <yonatancp.mgvc@gmail.com>
2024-10-17 11:40:24 -0500

    commit

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,38 +1,69 @@
 # TODO: Implementa el código del ejercicio aquí
-
 from abc import ABC, abstractmethod
+
+from validadorclave.modelo.errores import NoCumpleLongitudMinimaError, NoTieneLetraMayusculaError, \
+    NoTieneLetraMinusculaError, NoTieneNumeroError, NoTieneCaracterEspecialError, NoTienePalabraSecretaError
 
 
 class ReglaValidacion(ABC):
-    def __init__(self, longitud_esperada):
+    def __init__(self, longitud_esperada: int):
         self._longitud_esperada = longitud_esperada
 
-    @abstractmethod
-    def es_valida(self, clave):
-        pass
-
-    def _validar_longitud(self, clave):
+    def _validar_longitud(self, clave: str) -> bool:
         return len(clave) > self._longitud_esperada
 
-class ReglaLongitud(ReglaValidacion):
-    def __init__(self, longitud_esperada):
-        super().__init__(longitud_esperada)
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        return any(c.isupper() for c in clave)
 
-    def es_valida(self, clave):
-        return self._validar_longitud(clave)
+    def _contiene_minuscula(self, clave: str) -> bool:
+        return any(c.islower() for c in clave)
 
-    def _contiene_mayuscula(self):
+    def _contiene_numero(self, clave: str) -> bool:
+        return any(c.isdigit() for c in clave)
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
         pass
 
 
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        super().__init__(longitud_esperada=8)
 
-class Validador():
-    pass
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError("La clave no cumple con la longitud mínima.")
+        if not self._contiene_mayuscula(clave):
+            raise NoTieneLetraMayusculaError("La clave no contiene mayúsculas.")
+        if not self._contiene_minuscula(clave):
+            raise NoTieneLetraMinusculaError("La clave no contiene minúsculas.")
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError("La clave no contiene números.")
+        if not self.contiene_caracter_especial(clave):
+            raise NoTieneCaracterEspecialError("La clave no contiene un caracter especial.")
+        return True
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    def __init__(self):
+        super().__init__(longitud_esperada=6)
+
+    def contiene_calisto(self, clave: str) -> bool:
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError("La clave no cumple con la longitud mínima.")
+        if not self.contiene_calisto(clave):
+            raise NoTienePalabraSecretaError("La clave no contiene 'calisto' con las mayúsculas requeridas.")
+        return True
 
 
-class ReglaValidacionGanimedes():
-    pass
+class Validador:
+    def __init__(self, regla: ReglaValidacion):
+        self.regla = regla
 
-
-class ReglaValidacionCalisto():
-    pass
+    def es_valida(self, clave: str) -> bool:
+        return self.regla.es_valida(clave)
