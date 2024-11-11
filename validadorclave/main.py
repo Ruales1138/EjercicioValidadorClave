75fd87b28d8071bb098da692414214872f7d13cb
David Ruales <hruales1138@gmail.com>
2024-11-11 14:00:49 -0500

    Repaso

--- validadorclave/main.py
+++ validadorclave/main.py
@@ -1,22 +1,19 @@
 from validadorclave.modelo.errores import ValidadorError
-from validadorclave.modelo.validador import ReglaValidacion, Validador, ReglaValidacionCalisto
+from validadorclave.modelo.validador import ReglaValidacion, Validador, ReglaValidacionCalisto, ReglaValidacionGanimedes
 
 
 def validar_clave(clave: str, reglas: list[ReglaValidacion]):
     for regla in reglas:
-        validador: validador = Validador(regla)
+        validador: Validador = Validador(regla)
         try:
             validador.es_valida(clave)
         except ValidadorError as err:
             print(f'Error: {type(regla).__name__}: {err}')
-        else
+        else:
             print(f'INFO: La clave es valida segun la regla {type(regla).__name__}')
 
-
-class ReglaValidacionGaminedes:
-    pass
 
 
 if __name__ == '__main__':
     clave: str = input('Ingrese clave: ')
-    validar_clave(clave, [ReglaValidacionGaminedes(), ReglaValidacionCalisto()])
+    validar_clave(clave, [ReglaValidacionGanimedes(), ReglaValidacionCalisto()])
--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -7,29 +7,19 @@ from validadorclave.modelo.errores import NoCumpleLongitudMinimaError, NoTieneLe
 
 class ReglaValidacion(ABC):
     def __init__(self, longitud_esperada: int):
-        self._longitud_esperada = longitud_esperada
+        self._longitud_esperada: int = longitud_esperada
 
     def _validar_longitud(self, clave: str) -> bool:
         return len(clave) > self._longitud_esperada
 
-    def _contiene_mayuscula(self, clave: str) -> bool:
-        for letra in clave:
-            if letra.isupper():
-                return True
-        return False
+    def _contiene_mayuscula(self, clave: str):
+        return any(letra.isupper() for letra in clave)
 
+    def _contiene_minuscula(self, clave: str):
+        return any(letra.islower() for letra in clave)
 
-    def _contiene_minuscula(self, clave: str) -> bool:
-        for letra in clave:
-            if letra.islower():
-                return True
-        return False
-
-    def _contiene_numero(self, clave: str) -> bool:
-        for letra in clave:
-            if letra.isdigit():
-                return True
-        return False
+    def _contiene_numero(self, clave: str):
+        return any(letra.isdigit() for letra in clave)
 
     @abstractmethod
     def es_valida(self, clave: str) -> bool:
@@ -40,62 +30,60 @@ class ReglaValidacionGanimedes(ReglaValidacion):
     def __init__(self):
         super().__init__(longitud_esperada=8)
 
-    def contiene_caracter_especial(self, clave: str) -> bool:
-        for letra in clave:
-            if letra in "@_#$%":
-                return True
-        return False
+    def contiene_caracter_especial(self, clave: str):
+        return any(letra in '@_#$%' for letra in clave)
 
     def es_valida(self, clave: str) -> bool:
         if not self._validar_longitud(clave):
-            raise NoCumpleLongitudMinimaError("La clave debe tener una longitud de más de 8 caracteres")
+            raise NoCumpleLongitudMinimaError("La clave debe tener una longitud de más de 8 caracteres.")
         if not self._contiene_mayuscula(clave):
             raise NoTieneLetraMayusculaError("La clave no contiene mayúsculas.")
         if not self._contiene_minuscula(clave):
-            raise NoTieneLetraMinusculaError("La clave no contiene minúsculas.")
+            raise NoTieneLetraMinusculaError('La clave no contiene minúsculas.')
         if not self._contiene_numero(clave):
-            raise NoTieneNumeroError("La clave no contiene números.")
+            raise NoTieneNumeroError('La clave no contiene números.')
         if not self.contiene_caracter_especial(clave):
-            raise NoTieneCaracterEspecialError("La clave no contiene un caracter especial.")
+            raise NoTieneCaracterEspecialError('La clave no contiene un caracter especial.')
         return True
+
 
 class ReglaValidacionCalisto(ReglaValidacion):
     def __init__(self):
         super().__init__(longitud_esperada=6)
 
     def contiene_calisto(self, clave: str) -> bool:
-        clave_temp: str = clave.lower()
-        palabra: str = 'calisto'
+        palabra = 'calisto'
+        clave_temp = clave.lower()
         index_calisto = 0
+
         while (index_calisto + len(palabra)) <= len(clave):
             index_calisto = clave_temp.find(palabra, index_calisto)
-            # La clave contiene la palabra calisto
+
             if index_calisto >= 0:
                 pi = index_calisto
                 pf = index_calisto + len(palabra)
                 palabra_original = clave[pi:pf]
+                mayusculas = 0
 
-                contador_mayusculas = 0
                 for letra in palabra_original:
                     if letra.isupper():
-                        contador_mayusculas += 1
+                        mayusculas += 1
 
-                if 2 <= contador_mayusculas < len(palabra):
+                if 2 <= mayusculas < len(palabra):
                     return True
                 else:
-                    index_calisto += len(palabra)
+                    index_calisto = index_calisto + len(palabra)
             else:
                 return False
         return False
 
-
     def es_valida(self, clave: str) -> bool:
         if not self._validar_longitud(clave):
-            raise NoCumpleLongitudMinimaError("La clave no cumple con la longitud mínima.")
+            raise NoCumpleLongitudMinimaError('La clave no cumple con la longitud mínima.')
         if not self._contiene_numero(clave):
-            raise NoTieneNumeroError("La clave debe tener al menos un numero")
+            raise NoTieneNumeroError('La clave debe tener al menos un numero')
         if not self.contiene_calisto(clave):
-            raise NoTienePalabraSecretaError("La palabra calisto debe estar escrita con al menos dos letras en mayúscula")
+            raise NoTienePalabraSecretaError('La palabra calisto debe estar escrita con al menos dos letras en mayúscula')
         return True
 
 
